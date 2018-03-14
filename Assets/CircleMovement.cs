b76b19b0a8ab51f7c293f4b0c9a9a0f6d3e9e08f
Paul Indrome <fpschocke@gmail.com>
2018-03-14 23:01:35 +0100

    shader experiments, base animations, cinemachine

---
+++ Assets/CircleMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleMovement : MonoBehaviour
+{
+    public Transform target; 
+	public float speed = 30f; // speed in degrees per second 
+	public float gravity = 10f; // gravity, if you want to use it
+
+    CharacterController characterController;
+
+	public void Start(){
+		characterController = GetComponent<CharacterController>();
+
+	}
+
+	public void Update(){
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		if(new Vector2(horizontal, vertical).magnitude > 0.15f){
+			
+		}
+
+	}
+
+    // call this function in LateUpdate 
+	//void RotateChar(){ 
+	//	// make sure the variable "character" contains the controller: 
+	//	if (!character) character = GetComponent(CharacterController); 
+	//	// curPos = vector from target to character: 
+	//	Vector3 curPos = transform.position - target.position; 
+	//	// rotate curPos by the appropriate angle and save in newPos: 
+	//	Vector3 newPos = Quaternion.Euler(0, speed Time.deltaTime, 0) curPos; 
+	//	// calculate the displacement to move 
+	//	Vector3 displacement = newPos - curPos; 
+	//	// if you don't need gravity, comment out this line: 
+	//	displacement -= gravity Vector3.up Time.deltaTime * Time.deltaTime; 
+	// character.Move(displacement); 
+	//	// move by the distance calculated 
+	//	transform.LookAt(target); // keep looking to the target 
+	//}
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,13 +7,16 @@ public class PlayerMovement : MonoBehaviour {
 	public TargetDebug currentTarget;
 
 	public float radius, degreeSpeed, smoothTime;
+	float horizontal, vertical;
 	float targetAngle, currentAngle, angleDifference, nextAngle, currentCircleVelocity;
 	Quaternion rotationDifference;
-	Vector3 targetPositionOnCircle, nextPos;
+	Vector3 targetPositionOnCircle, nextPos, yRaise;
 	CharacterController characterController;
 	Coroutine lerpToAngleCor;
 
 	LineRenderer lineRenderer;
+
+	public Animator playerAnimator;
 
 	// Use this for initialization
 	void Start () {
@@ -26,28 +29,34 @@ public class PlayerMovement : MonoBehaviour {
 
 		currentTarget.posChangeEvent += TargetPositionUpdate;
 
+		yRaise = new Vector3(0,1,0);
+
 		characterController = GetComponent<CharacterController>();
 		lineRenderer = GetComponent<LineRenderer>();
-		lineRenderer.SetPosition(0, currentTarget.transform.position);
+		lineRenderer.SetPosition(0, currentTarget.transform.position + yRaise);
 		currentAngle = Vector3.SignedAngle(Vector3.forward, transform.position, Vector3.up);
-		
+		targetAngle = currentAngle;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float horizontal = Input.GetAxis("Horizontal");
-		float vertical = Input.GetAxis("Vertical");
+		horizontal = Input.GetAxis("Horizontal");
+		vertical = Input.GetAxis("Vertical");
 
-		if(new Vector2(horizontal, vertical).magnitude > 0.8f){
-			targetPositionOnCircle = new Vector3(horizontal, 0, vertical);
-			targetAngle = Vector3.SignedAngle(Vector3.forward, targetPositionOnCircle, Vector3.up);
-			targetPositionOnCircle = targetPositionOnCircle.normalized * radius;
-			lineRenderer.SetPosition(2, currentTarget.transform.position + targetPositionOnCircle);
+		if(new Vector2(horizontal, vertical).magnitude > 0.15f){
+			//targetPositionOnCircle = new Vector3(horizontal, 0, vertical);
+			//targetAngle = Vector3.SignedAngle(Vector3.forward, targetPositionOnCircle, Vector3.up);
+			//targetPositionOnCircle = targetPositionOnCircle.normalized * radius;
+			
 			//angleDifference = Vector3.SignedAngle(transform.position, targetPositionOnCircle, Vector3.up);
 			//rotationDifference = Quaternion.AngleAxis(angleDifference, Vector3.up);
-			if(lerpToAngleCor == null)
-				lerpToAngleCor = StartCoroutine(LerpToAnglePosition());
+			targetAngle -= 2 * (degreeSpeed * Mathf.Abs(horizontal)) * Time.deltaTime * Mathf.Sign(horizontal);
+			//lineRenderer.SetPosition(2, currentTarget.transform.position + targetPositionOnCircle + yRaise);
+			lineRenderer.SetPosition(2, radius * new Vector3(Mathf.Sin(Mathf.Deg2Rad * targetAngle), 0, Mathf.Cos(Mathf.Deg2Rad * targetAngle)) + yRaise);
+
 		}
+
+		LerpToAnglePosition(); 
 
 		//nextAngle = Mathf.MoveTowardsAngle(currentAngle, targetAngle, Time.deltaTime * degreeSpeed);
 		//nextAngle = Mathf.SmoothDampAngle(currentAngle, targetAngle, ref currentCircleVelocity, smoothTime, degreeSpeed, Time.deltaTime * 2);
@@ -58,32 +67,26 @@ public class PlayerMovement : MonoBehaviour {
 	}
 
 	void TargetPositionUpdate(Vector3 newPosition){
-		lineRenderer.SetPosition(0, newPosition);
+		lineRenderer.SetPosition(0, newPosition + yRaise);
 	}
 
-	IEnumerator LerpToAnglePosition(){
-		while(Mathf.Abs(targetAngle - currentAngle) > 0.25f){
-			nextAngle = Mathf.SmoothDampAngle(currentAngle, targetAngle, ref currentCircleVelocity, smoothTime, degreeSpeed, Time.deltaTime * 2);
-			
-			nextPos = (radius * new Vector3(Mathf.Sin(Mathf.Deg2Rad * nextAngle), 0, Mathf.Cos(Mathf.Deg2Rad * nextAngle)));
-
-			lineRenderer.SetPosition(1, currentTarget.transform.position + nextPos);
-
-			currentAngle = Vector3.SignedAngle(Vector3.forward, nextPos, Vector3.up);
-
-			characterController.Move(currentTarget.transform.position + nextPos - transform.position);
-			yield return null;
-		}
-
-		lineRenderer.SetPosition(1, currentTarget.transform.position + targetPositionOnCircle);
-
-		characterController.Move(currentTarget.transform.position + (targetPositionOnCircle - transform.position));
-
-		lerpToAngleCor = null;
-
-		Debug.Log("Lerping stopped");
-
-		yield return null;
+	public void LerpToAnglePosition(){
+		nextAngle = Mathf.SmoothDampAngle(currentAngle, targetAngle, ref currentCircleVelocity, smoothTime, degreeSpeed, Time.deltaTime * 2);
+		playerAnimator.SetFloat("speed", Mathf.Sqrt(Mathf.Abs(currentCircleVelocity)) * -Mathf.Sign(currentCircleVelocity));
 		
+		nextPos = (radius * new Vector3(Mathf.Sin(Mathf.Deg2Rad * nextAngle), 0, Mathf.Cos(Mathf.Deg2Rad * nextAngle)));
+
+		lineRenderer.SetPosition(1, currentTarget.transform.position + nextPos + yRaise);
+
+		currentAngle = Vector3.SignedAngle(Vector3.forward, nextPos, Vector3.up);
+
+		characterController.Move((currentTarget.transform.position + nextPos) - transform.position);
+		transform.LookAt(currentTarget.transform.position);
+
+		//playerAnimator.SetFloat("speed", 0);
+
+		//lineRenderer.SetPosition(1, currentTarget.transform.position + targetPositionOnCircle + yRaise);
+
+		//characterController.Move((currentTarget.transform.position + targetPositionOnCircle) - transform.position);
 	}
 }
